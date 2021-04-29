550613f6e5e20cbe9171243f2e319a4926838a19
xgao <1069379433@qq.com>
2021-04-29 06:39:39 +0200

    HLTValidation update for plots

--- rootScripts/makeDQMHLTRelValPlots.C
+++ rootScripts/makeDQMHLTRelValPlots.C
@@ -3,15 +3,18 @@
 #include "TLegend.h"
 #include "TStyle.h"
 #include "TPaveStats.h"
+#include "TPaveText.h"
 #include "TFile.h"
 #include "TH1.h"
 #include "TH2.h"
+#include "TF1.h"
 #include "TGraph.h"
 #include "TROOT.h"
 #include "TKey.h"
 #include "TPaletteAxis.h"
 #include "TCanvas.h"
 #include "TSystem.h"
+#include "TMath.h"
 #include <iostream>
 
 namespace util {
@@ -51,6 +54,23 @@ struct ValData {
   {}
 };
 
+std::string check_run_no(const std::string& filename)
+{
+     TFile *f1 = TFile::Open(filename.c_str(),"READ");
+     TDirectory* dir = (TDirectory*)f1->Get("DQMData");
+     TIter next(dir->GetListOfKeys());
+     TKey *key;
+     while ((key = (TKey*)next())) {
+       //cout<<key->GetName()<<endl;
+       if(strstr(key->GetName(),"Run "))
+       {
+       	 return key->GetName();
+       }
+     }
+     return "Run 1";
+}
+
+
 TH2* ratioInSigma(TH2* numer,TH2* denom)
 {
   TH2* ratio = static_cast<TH2*>(numer->Clone("ratio"));
@@ -79,17 +99,48 @@ TH2* ratioInSigma(TH2* numer,TH2* denom)
 }
 
 
+float getMaximum(TH1* h1)
+{
+  float tmp_max = 0;
+  for(Int_t ii=1;ii<h1->GetNbinsX()+1;ii++)
+  {
+    if(tmp_max < h1->GetBinContent(ii))
+    {
+      tmp_max = h1->GetBinContent(ii);
+    }
+  }
+  return tmp_max;
+}
+
+float getMinimum(TH1* h1)
+{
+  float tmp_min = 1.0;
+  for(Int_t ii=1;ii<h1->GetNbinsX()+1;ii++)
+  {
+    if(tmp_min > h1->GetBinContent(ii))
+    {
+      tmp_min = h1->GetBinContent(ii);
+    }
+  }
+  return tmp_min;
+}
 
 TCanvas* makeTPPlot(const std::string& prefix,const std::string& path,const std::string& filter,const ValData& valData)
 { 
-  std::string baseDir = "DQMData/Run 1/HLT/Run summary/EGTagAndProbeEffs";
+  std::string baserun_no = check_run_no(valData.filename);
+  std::string refbaserun_no = check_run_no(valData.refFilename);
+  std::string baseDir = "DQMData/"+baserun_no+"/HLT/Run summary/EGM/TagAndProbeEffs";
+  std::string refbaseDir = "DQMData/"+refbaserun_no+"/HLT/Run summary/EGM/TagAndProbeEffs";
+  std::cout<<baseDir<<std::endl;
+  std::cout<<refbaseDir<<std::endl;
+//  std::string refbaseDir = "DQMData/Run 1/HLT/Run summary/EGTagAndProbeEffs";
   std::vector<std::string> suffexes ={"_EEvsEt_eff","_EBvsEt_eff","_EEvsPhi_eff","_EBvsPhi_eff","_vsSCEtaPhi_eff","_vsSCEta_eff"};
   if(prefix=="eleWPTightTagPhoHighEtaProbe"){
     suffexes ={"_vsEt_eff","_vsPhi_eff","_vsSCEtaPhi_eff","_vsSCEta_eff"};
   }
 
   TCanvas* c1 = static_cast<TCanvas*>(gROOT->FindObject("effCanvas"));
-  if(!c1) c1 = new TCanvas("effCanvas","",900*1.5,600);
+  if(!c1) c1 = new TCanvas("effCanvas","",900*1.5,800);
   c1->cd();
 
   int minEntries=std::numeric_limits<int>::max();
@@ -97,9 +148,12 @@ TCanvas* makeTPPlot(const std::string& prefix,const std::string& path,const std:
     std::string suffex;
     if(histNr<suffexes.size()) suffex = suffexes[histNr];
     std::string histName = baseDir+"/"+path+"/"+prefix+"_"+filter+suffex;
+    std::string refhistName = refbaseDir+"/"+path+"/"+prefix+"_"+filter+suffex;
+    //std::cout<<"#########################"<<histName<<std::endl;
+    //std::cout<<"#########################"<<refhistName<<std::endl;
     
     TH1* hist = util::getHist(histName,valData.filename); 
-    TH1* histRef = util::getHist(histName,valData.refFilename); 
+    TH1* histRef = util::getHist(refhistName,valData.refFilename); 
     float yOffset = ((histNr)%6)%2 * 0.5;
     float xOffset = ((histNr)%6)/2 * 0.33;
 
@@ -110,59 +164,127 @@ TCanvas* makeTPPlot(const std::string& prefix,const std::string& path,const std:
     if(hist){
       if(hist->GetEntries()<minEntries) minEntries=hist->GetEntries(); 
       if(hist->ClassName()==std::string("TH2F")){
-	//	hist->GetZaxis()->SetRangeUser(0,1);
-	hist->Draw("COLZ");
-	histPad->SetMargin(0.13,0.114,0.1005,0.09);
-	histPad->Update();
+        TPad* histPad = new TPad("histPad","",xOffset,yOffset,0.33+xOffset,0.5+yOffset);
+        c1->cd();
+        histPad->Draw();
+        histPad->cd();
 
-	TPaletteAxis *palette = 
-	  static_cast<TPaletteAxis*>(hist->GetListOfFunctions()->FindObject("palette"));
-	if(palette){
-	  palette->SetX1NDC(0.890927);
-	  palette->SetX2NDC(0.935887);
-	  palette->SetY1NDC(0.108);
-	  palette->SetY2NDC(0.912892);
-	}
-
-	hist = ratioInSigma(static_cast<TH2*>(hist),
-			    static_cast<TH2*>(histRef));
-
-
-	hist->Draw("COLZ");
-
-      }else{
-	hist->GetYaxis()->SetRangeUser(0,1.05);
-	hist->GetYaxis()->SetNdivisions(510);
-	
-	hist->SetMarkerStyle(8); 
-	hist->SetMarkerColor(4); 
-	//	hist->SetMarkerSize(0.7); 
-	hist->SetLineColor(4);
-
-	hist->Draw();
-	histPad->SetGridx();
-	histPad->SetGridy();
-
-	TLegend* leg = new TLegend(0.305728,0.142857,0.847496,0.344948);
-	leg->SetFillStyle(0);
-	leg->SetBorderSize(0);
-	leg->AddEntry(hist,valData.legEntry.c_str(),"LP");
-	
-	if(histRef){
-	  histRef->SetMarkerStyle(4); 
-	  histRef->SetMarkerColor(2); 
-	  //	hist->SetMarkerSize(0.7); 
-	  histRef->SetLineColor(2);
-	  histRef->Draw("SAME");
-	  leg->AddEntry(histRef,valData.refLegEntry.c_str(),"LP");
-	  
-	}else{
-	  leg->AddEntry(histRef,valData.refLegEntry.c_str(),"");
-	}
-	leg->Draw();
+      	//	hist->GetZaxis()->SetRangeUser(0,1);
+        hist->Draw("COLZ");
+      	histPad->SetMargin(0.13,0.114,0.1005,0.09);
+      	histPad->Update();
+      
+      	TPaletteAxis *palette = 
+      	  static_cast<TPaletteAxis*>(hist->GetListOfFunctions()->FindObject("palette"));
+      	if(palette){
+      	  palette->SetX1NDC(0.890927);
+      	  palette->SetX2NDC(0.935887);
+      	  palette->SetY1NDC(0.108);
+      	  palette->SetY2NDC(0.912892);
+      	}
+      
+      	hist = ratioInSigma(static_cast<TH2*>(hist),
+      			    static_cast<TH2*>(histRef));
+      
+      
+      	hist->Draw("COLZ");
+      }
+      else{
+        TPad* histPad = new TPad("histPad","",xOffset,0.15+yOffset,0.33+xOffset,0.5+yOffset);
+        TPad* histPad_ratio = new TPad("histPad_ratio","",xOffset,yOffset,0.33+xOffset,0.15+yOffset);
+        histPad->SetBottomMargin(0.0);
+        histPad_ratio->SetTopMargin(0.0);
+        histPad_ratio->SetBottomMargin(0.4);
+        c1->cd();
+        histPad->Draw();
+        histPad_ratio->Draw();
+        histPad->cd();
+      	hist->GetYaxis()->SetRangeUser(0,1.05);
+      	hist->GetYaxis()->SetNdivisions(510);
+	     
+      	hist->SetMarkerStyle(8); 
+      	hist->SetMarkerColor(4); 
+      	//	hist->SetMarkerSize(0.7); 
+      	hist->SetLineColor(4);
+      
+      	hist->Draw();
+      	histPad->SetGridx();
+      	histPad->SetGridy();
+      
+      	TLegend* leg = new TLegend(0.305728,0.142857,0.847496,0.344948);
+      	leg->SetFillStyle(0);
+      	leg->SetBorderSize(0);
+      	leg->AddEntry(hist,valData.legEntry.c_str(),"LP");
+	     
+      	if(histRef){
+      	  histRef->SetMarkerStyle(4); 
+      	  histRef->SetMarkerColor(2); 
+      	  //	hist->SetMarkerSize(0.7); 
+      	  histRef->SetLineColor(2);
+      	  histRef->Draw("SAME");
+      	  leg->AddEntry(histRef,valData.refLegEntry.c_str(),"LP"); 
+      	}
+        else{
+      	  leg->AddEntry(histRef,valData.refLegEntry.c_str(),"");
+      	}
+      	leg->Draw();
+        if(histRef){
+          histPad_ratio->cd();
+          TH1* hist_ratio = (TH1F*)hist->Clone("hist_ratio");
+          hist_ratio->Divide(histRef);
+          hist_ratio->SetTitle("");
+          hist_ratio->GetYaxis()->SetTitle("Ratio");
+          hist_ratio->GetXaxis()->SetTitleSize(0.058/0.3);
+          hist_ratio->GetYaxis()->SetTitleSize(0.045/0.3);
+          hist_ratio->GetXaxis()->SetTitleFont(42);
+          hist_ratio->GetYaxis()->SetTitleFont(42);
+          hist_ratio->GetXaxis()->SetTickLength(0.05);
+          hist_ratio->GetYaxis()->SetTickLength(0.05);
+          hist_ratio->GetXaxis()->SetLabelSize(0.045/0.3);
+          hist_ratio->GetYaxis()->SetLabelSize(0.04/0.3);
+          hist_ratio->GetXaxis()->SetLabelOffset(0.02);
+          hist_ratio->GetYaxis()->SetLabelOffset(0.01);
+          hist_ratio->GetYaxis()->SetTitleOffset(0.41);
+          hist_ratio->GetXaxis()->SetTitleOffset(0.23/0.25);
+//          hist_ratio->GetYaxis()->SetNdivisions(304);
+          hist_ratio->SetLineWidth(2);
+          float y_axis_max = 1.5;
+          float y_axis_min = 0.5;
+          hist_ratio->Draw();
+          TF1 *f1 = new TF1("f1", "[0]");
+          Int_t fitStatus = hist_ratio->Fit("f1","O");
+          if(fitStatus == 0)
+          {
+            float chi2 = hist_ratio->GetFunction("f1")->GetChisquare();
+            char str_chi2[30];
+            //sprintf(str_chi2,"#chi^{2} : %0.2f",chi2);
+            sprintf(str_chi2,"#chi^{2} : %0.2f, Prob : %0.2f",chi2,TMath::Prob(chi2,hist_ratio->GetNbinsX()-1));
+            TPaveText* Text_fit= new TPaveText(0.55,0.7, 1.0,1.0,"blNDC");
+            Text_fit->SetBorderSize(0);
+            Text_fit->SetFillStyle(0);
+            Text_fit->SetTextAlign(10);
+            Text_fit->SetTextColor(1);
+            Text_fit->SetTextSize(0.12);
+            Text_fit->AddText(str_chi2);
+            Text_fit->Draw();
+            if(y_axis_min < getMinimum(hist_ratio))
+            {
+            	y_axis_min = 0.95*getMinimum(hist_ratio);
+            }
+            if(y_axis_max > getMaximum(hist_ratio))
+            {
+            	y_axis_max = 1.05*getMaximum(hist_ratio);
+            }
+            std::cout<<"y_axis_min : "<<y_axis_min<<std::endl;
+            std::cout<<"y_axis_max : "<<y_axis_max<<std::endl;
+            hist_ratio->SetMaximum(1.05*y_axis_max);
+            hist_ratio->SetMinimum(0.95*y_axis_min);
+          }
+        }
       }
       
-    }else{
+    }
+    else{
       std::cout <<"histName"<<histName<<"not found"<<std::endl;
     }
   }
@@ -190,6 +312,7 @@ void printAllTPPlots(const std::string& dir,const ValData& valData)
     {"eleWPTightTag","HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL","hltEle23Ele12CaloIdLTrackIdLIsoVLTrackIsoLeg2Filter"},
     {"eleWPTightTag","HLT_Ele27_WPTight_Gsf","hltEle27WPTightGsfTrackIsoFilter"},
     {"eleWPTightTag","HLT_Ele32_WPTight_Gsf","hltEle32WPTightGsfTrackIsoFilter"},
+    {"eleWPTightTag","HLT_Ele32_WPTight_Gsf","hltEle32WPTightEcalIsoFilterforTau"},
     {"eleWPTightTag","HLT_Ele35_WPTight_Gsf","hltEle35noerWPTightGsfTrackIsoFilter"},
     {"eleWPTightTag","HLT_Ele38_WPTight_Gsf","hltEle38noerWPTightGsfTrackIsoFilter"},
     {"eleWPTightTag","HLT_Ele32_WPTight_Gsf_L1DoubleEG","hltEle32L1DoubleEGWPTightGsfTrackIsoFilter"},
@@ -305,7 +428,11 @@ std::vector<std::string> getPathNames(TDirectory* dir,const std::string& dirName
 
 void printNonTPDQM(const std::string& plotDir,const ValData& valData)
 {
-  std::string dir="DQMData/Run 1/HLT/Run summary/EgOffline/Source_Histos";
+  std::string baserun_no = check_run_no(valData.filename);
+  std::string refbaserun_no = check_run_no(valData.refFilename);
+  std::string dir="DQMData/"+baserun_no+"/HLT/Run summary/EGM/Source_Histos";
+  std::string refdir="DQMData/"+refbaserun_no+"/HLT/Run summary/EGM/Source_Histos";
+//  std::string refdir="DQMData/Run 1/HLT/Run summary/EgOffline/Source_Histos";
   std::vector<std::string> paths={"hltEle23Ele12CaloIdLTrackIdLIsoVLTrackIsoLeg1Filter",
 				  "hltEle27WPTightGsfTrackIsoFilter",
 				  "hltEle115CaloIdVTGsfTrkIdTGsfDphiFilter",
@@ -322,7 +449,7 @@ void printNonTPDQM(const std::string& plotDir,const ValData& valData)
     file->cd();
     for(const auto& histName : histNames){
       auto hist = util::getHist(dir+"/"+path+"/"+histName,valData.filename);
-      auto histRef = util::getHist(dir+"/"+path+"/"+histName,valData.refFilename);
+      auto histRef = util::getHist(refdir+"/"+path+"/"+histName,valData.refFilename);
       
       if(hist){
 	util::setHistAttributes(hist,4,1,8,4);
@@ -343,13 +470,15 @@ void printNonTPDQM(const std::string& plotDir,const ValData& valData)
 
 void printValidationDQM(const std::string& plotDir,const ValData& valData)
 {
+  std::string run_no = check_run_no(valData.filename);
   gSystem->mkdir((plotDir+"/EGValidation").c_str(),true);
-  std::string dir="DQMData/Run 1/HLT/Run summary/HLTEgammaValidation";
+  std::string dir="DQMData/"+run_no+"/HLT/Run summary/HLTEgammaValidation";
   std::vector<std::string> histNames={"final_eff_vs_et","final_eff_vs_eta"};
   
   TFile* file = TFile::Open(valData.filename.c_str(),"READ");
   
   file->cd(dir.c_str());
+  std::cout<<dir<<std::endl;
   std::vector<std::string> pathNames = getPathNames(file,dir);
   file->cd();
   for(const auto& path : pathNames){
@@ -360,11 +489,21 @@ void printValidationDQM(const std::string& plotDir,const ValData& valData)
     for(const auto& histName : histNames){
       auto hist = util::getHist(dir+"/"+path+"/"+histName,valData.filename);
       auto histRef = util::getHist(dir+"/"+path+"/"+histName,valData.refFilename);
-      
-      if(hist){
-	hist->SetTitle((path+" "+hist->GetTitle()).c_str());
-	util::setHistAttributes(hist,4,1,8,4);
-	hist->Draw();
+
+  TCanvas* c1 = static_cast<TCanvas*>(gROOT->FindObject("c1"));      
+  if(hist){
+        TPad* histPad = new TPad("histPad","",0,0.3,1.0,1.0);
+        TPad* histPad_ratio = new TPad("histPad_ratio","",0,0,1.0,0.3);
+        histPad->SetBottomMargin(0.0);
+        histPad_ratio->SetTopMargin(0.0);
+        histPad_ratio->SetBottomMargin(0.4);
+        c1->cd();
+        histPad->Draw();
+        histPad_ratio->Draw();
+        histPad->cd();
+	      hist->SetTitle((path+" "+hist->GetTitle()).c_str());
+	      util::setHistAttributes(hist,4,1,8,4);
+	      hist->Draw();
 
 	
 	TLegend* leg = new TLegend(0.305728,0.142857,0.847496,0.344948);
@@ -376,13 +515,64 @@ void printValidationDQM(const std::string& plotDir,const ValData& valData)
 	  util::setHistAttributes(histRef,2,1,4,2);
 	  histRef->Draw("SAME");  
 	  leg->AddEntry(histRef,valData.refLegEntry.c_str(),"LP");
-	}
+  }
+  leg->Draw();
+  if(histRef){
+          histPad_ratio->cd();
+          TH1* hist_ratio = (TH1F*)hist->Clone("hist_ratio");
+          hist_ratio->Divide(histRef);
+          hist_ratio->SetTitle("");
+          hist_ratio->GetYaxis()->SetTitle("Ratio");
+          hist_ratio->GetXaxis()->SetTitleSize(0.058/0.3);
+          hist_ratio->GetYaxis()->SetTitleSize(0.045/0.3);
+          hist_ratio->GetXaxis()->SetTitleFont(42);
+          hist_ratio->GetYaxis()->SetTitleFont(42);
+          hist_ratio->GetXaxis()->SetTickLength(0.05);
+          hist_ratio->GetYaxis()->SetTickLength(0.05);
+          hist_ratio->GetXaxis()->SetLabelSize(0.045/0.3);
+          hist_ratio->GetYaxis()->SetLabelSize(0.04/0.3);
+          hist_ratio->GetXaxis()->SetLabelOffset(0.02);
+          hist_ratio->GetYaxis()->SetLabelOffset(0.01);
+          hist_ratio->GetYaxis()->SetTitleOffset(0.41);
+          hist_ratio->GetXaxis()->SetTitleOffset(0.23/0.25);
+//          hist_ratio->GetYaxis()->SetNdivisions(304);
+          hist_ratio->SetLineWidth(2);
+          float y_axis_max = 1.5;
+          float y_axis_min = 0.5;
+          hist_ratio->Draw();
+          TF1 *f1 = new TF1("f1", "[0]");
+          Int_t fitStatus = hist_ratio->Fit("f1","O");
+          if(fitStatus == 0)
+          {
+            float chi2 = hist_ratio->GetFunction("f1")->GetChisquare();
+            char str_chi2[30];
+            sprintf(str_chi2,"#chi^{2} : %0.2f, Prob : %0.2f",chi2,TMath::Prob(chi2,hist_ratio->GetNbinsX()-1));
+            TPaveText* Text_fit= new TPaveText(0.55,0.7, 1.0,1.0,"blNDC");
+            Text_fit->SetBorderSize(0);
+            Text_fit->SetFillStyle(0);
+            Text_fit->SetTextAlign(10);
+            Text_fit->SetTextColor(1);
+            Text_fit->SetTextSize(0.12);
+            Text_fit->AddText(str_chi2);
+            Text_fit->Draw();
+            if(y_axis_min < getMinimum(hist_ratio))
+            {
+            	y_axis_min = 0.95*getMinimum(hist_ratio);
+            }
+            if(y_axis_max > getMaximum(hist_ratio))
+            {
+            	y_axis_max = 1.05*getMaximum(hist_ratio);
+            }
+            std::cout<<"y_axis_min : "<<y_axis_min<<std::endl;
+            std::cout<<"y_axis_max : "<<y_axis_max<<std::endl;
+            hist_ratio->SetMaximum(1.05*y_axis_max);
+            hist_ratio->SetMinimum(0.95*y_axis_min);
+          }
+  	}
 
-	leg->Draw();
-	TCanvas* c1 = static_cast<TCanvas*>(gROOT->FindObject("c1"));
 	c1->Update();
 	util::printCanvas(plotDir+"/EGValidation/"+path+"_"+histName,c1);
-      }
+  }
 
     }
 	
